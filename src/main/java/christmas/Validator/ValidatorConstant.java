e2253cffebfa51610ae823528e269df7655f8d5d
unifolio0 <boho82ha@gmail.com>
2023-11-10 15:48:45 +0900

    refactor(ValidatorConstant) : 같은 오류메시지 출력 상수 통합
    - ValidatorConstant : PARSE_INT_ERROR와 VISIT_DATE_RANGE_ERROR를 VISIT_DATE_ERROR로 통합

--- src/main/java/christmas/Validator/ValidatorConstant.java
+++ src/main/java/christmas/Validator/ValidatorConstant.java
@@ -1,8 +1,8 @@
 package christmas.Validator;
 
 public enum ValidatorConstant {
-    PARSE_INT_ERROR("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    VISIT_DATE_RANGE_ERROR("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    VISIT_DATE_ERROR("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    MENU_AND_COUNT_ERROR("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String message;
 
