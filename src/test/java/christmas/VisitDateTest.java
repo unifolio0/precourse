b55b6eaf9f2ae7c2a6adfa0e8614cf37749fb1e0
unifolio0 <boho82ha@gmail.com>
2023-11-12 10:39:59 +0900

    style : 좀 더 가독성 있도록 수정
    - 테스트 케이스를 그룹별로 구분한 뒤 설명 추가

--- src/test/java/christmas/VisitDateTest.java
+++ src/test/java/christmas/VisitDateTest.java
@@ -6,10 +6,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class VisitDateTest {
-    @DisplayName("방문 날짜 입력 예외 처리 -> 1~3: 1~31의 범위를 벗어나는 경우, 4~7: 정수가 아닌 경우")
-    @ValueSource(strings = {"32", "0", "-12", "f", "12f", "1@", "1.1"})
+    @DisplayName("방문 날짜 입력 예외 처리")
+    @ValueSource(strings = {
+            "32", "0", "-12",    /*1~31의 범위를 벗어나는 경우*/
+            "f", "12f", "1@", "1.1"    /*정수가 아닌 경우*/
+    })
     @ParameterizedTest
     void underLottoPrice(String input) {
         assertThatThrownBy(() -> new VisitDate(input))
