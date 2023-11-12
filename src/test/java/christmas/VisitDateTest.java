dcb36091414cd3c9df615c542abdb2b7316b2289
unifolio0 <boho82ha@gmail.com>
2023-11-12 10:58:31 +0900

    style(VisitDateTest) : 테스트 설명 추가

--- src/test/java/christmas/VisitDateTest.java
+++ src/test/java/christmas/VisitDateTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class VisitDateTest {
-    @DisplayName("방문 날짜 입력 예외 처리")
+    @DisplayName("유효하지 않은 방문 날짜 입력 예외 처리")
     @ValueSource(strings = {
             "32", "0", "-12",    /*1~31의 범위를 벗어나는 경우*/
             "f", "12f", "1@", "1.1"    /*정수가 아닌 경우*/
