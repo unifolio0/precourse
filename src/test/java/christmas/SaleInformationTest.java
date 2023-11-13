b672628045277bb88d6e8bf1766ed712a765b7b3
unifolio0 <boho82ha@gmail.com>
2023-11-13 15:13:55 +0900

    test(SaleInformationTest) : 테스트 케이스 추가
    - 혜택 내역에 대한 테스트 케이스 추가 및 설명 추가

--- src/test/java/christmas/SaleInformationTest.java
+++ src/test/java/christmas/SaleInformationTest.java
@@ -28,7 +28,7 @@ public class SaleInformationTest {
 
     @DisplayName("혜택 내역을 정확히 출력하는지 테스트")
     @Test
-    void detailsMentCheck1() {
+    void detailsMentCheck1() {/*크리스마스 디데이 할인, 주말 할인을 받는 경우: 주말(1일)에 메인 메뉴 1개 포함 120,000원 미만 주문*/
         String input = "양송이수프-2,티본스테이크-1,초코케이크-1";
         SaleInformation saleInformation = new SaleInformation(1, new User(input));
         assertThat((saleInformation.allDetailsMent()))
@@ -36,7 +36,7 @@ public class SaleInformationTest {
     }
 
     @Test
-    void detailsMentCheck2() {
+    void detailsMentCheck2() {/*크리스마스 디데이 할인, 주말 할인, 증정 이벤트를 받는 경우: 주말(16일)에 메인 메뉴 3개 포함 120,000원 이상 주문*/
         String input = "양송이수프-2,티본스테이크-3,아이스크림-1";
         SaleInformation saleInformation = new SaleInformation(16, new User(input));
         assertThat((saleInformation.allDetailsMent()))
@@ -44,23 +44,23 @@ public class SaleInformationTest {
     }
 
     @Test
-    void detailsMentCheck3() {
-        String input = "양송이수프-2,티본스테이크-3,아이스크림-1";
+    void detailsMentCheck3() {/*주말 할인, 증정 이벤트를 받는 경우: 25일 이후 주말(29일)에 메인 메뉴 3개 포함 120,000원 이상 주문*/
+        String input = "양송이수프-2,티본스테이크-1,바비큐립-2,아이스크림-1";
         SaleInformation saleInformation = new SaleInformation(29, new User(input));
         assertThat((saleInformation.allDetailsMent()))
                 .isEqualTo("주말 할인: -6,069원\n증정 이벤트: -25,000원\n");
     }
 
     @Test
-    void detailsMentCheck4() {
+    void detailsMentCheck4() {/*할인 혜택이 없는 경우: 25일 이후 주말(29일)에 메인 메뉴 없이 120,000원 미만 주문*/
         String input = "양송이수프-2,초코케이크-3,아이스크림-1";
         SaleInformation saleInformation = new SaleInformation(29, new User(input));
         assertThat((saleInformation.allDetailsMent()))
-                .isEqualTo("없음");
+                .isEqualTo("없음\n");
     }
 
     @Test
-    void detailsMentCheck5() {
+    void detailsMentCheck5() {/*크리스마스 디데이 할인, 특별 할인 받는 경우: 별표날(10일)에 디저트 없이 120,000원 미만 주문*/
         String input = "양송이수프-2,티본스테이크-1,샴페인-1";
         SaleInformation saleInformation = new SaleInformation(10, new User(input));
         assertThat((saleInformation.allDetailsMent()))
@@ -68,7 +68,7 @@ public class SaleInformationTest {
     }
 
     @Test
-    void detailsMentCheck6() {
+    void detailsMentCheck6() {/*크리스마스 디데이 할인, 평일 할인, 특별 할인 받는 경우: 별표날(25일)에 디저트 2개 포함 120,000원 미만 주문*/
         String input = "양송이수프-2,티본스테이크-1,초코케이크-2,제로콜라-1";
         SaleInformation saleInformation = new SaleInformation(25, new User(input));
         assertThat((saleInformation.allDetailsMent()))
@@ -76,10 +76,26 @@ public class SaleInformationTest {
     }
 
     @Test
-    void detailsMentCheck7() {
-        String input = "시저샐러드-4,초코케이크-5,레드와인-1";
+    void detailsMentCheck7() {/*평일 할인, 특별 할인, 증정 이벤트를 받는 경우: 25일 이후 별표날(31일)에 디저트 5개 포함 120,000원 이상 주문한 경우*/
+        String input = "시저샐러드-4,초코케이크-2,레드와인-1,아이스크림-3";
         SaleInformation saleInformation = new SaleInformation(31, new User(input));
         assertThat((saleInformation.allDetailsMent()))
                 .isEqualTo("평일 할인: -10,115원\n특별 할인: -1,000원\n증정 이벤트: -25,000원\n");
+    }
+
+    @Test
+    void detailsMentCheck8() {/*할인 혜택이 없는 경우: 10,000원 미만 주문한 경우*/
+        String input = "양송이수프-1";
+        SaleInformation saleInformation = new SaleInformation(3, new User(input));
+        assertThat((saleInformation.allDetailsMent()))
+                .isEqualTo("없음\n");
+    }
+
+    @Test
+    void detailsMentCheck9() {/*증명 이벤트 받는 경우: 25일 이후 평일(28일)에 디저트 없이 120,000원 이상 주문한 경우*/
+        String input = "양송이수프-1,크리스마스파스타-4,레드와인-1";
+        SaleInformation saleInformation = new SaleInformation(28, new User(input));
+        assertThat((saleInformation.allDetailsMent()))
+                .isEqualTo("증정 이벤트: -25,000원\n");
     }
 }
