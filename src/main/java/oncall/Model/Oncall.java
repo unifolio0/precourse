ee113424109a0588eec685b3d79c45f01432c5c7
unifolio0 <boho82ha@gmail.com>
2023-12-16 17:13:23 +0900

    feat : 순번 짜는 기능 구현 - 2
    - Oncall : 각 날에 맞는 출력 형식을 생성하는 메소드 구현, 해당 날짜에 해당하는 사람의 인덱스를 구하는 메소드 구현, 해당 날짜의 사람을 구하는 메소드 구현, 해당 날짜가 휴일인지 확인하는 메소드 구현

--- src/main/java/oncall/Model/Oncall.java
+++ src/main/java/oncall/Model/Oncall.java
@@ -1,10 +1,87 @@
 package oncall.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Oncall {
+    private MonthDay monthDay;
     private Peoples dayPeoples;
     private Peoples holidayPeoples;
-    public Oncall(Peoples dayPeoples, Peoples holidayPeoples) {
+    private Map<Integer, Map<Day, Boolean>> calendar;
+    private int dayidx, holidayidx;
+    private boolean isday = false, isholiday = false;
+    private String prePeople;
+
+    public Oncall(MonthDay monthDay, Peoples dayPeoples, Peoples holidayPeoples) {
+        this.monthDay = monthDay;
         this.dayPeoples = dayPeoples;
         this.holidayPeoples = holidayPeoples;
+        this.calendar = this.monthDay.getCalendar();
+    }
+
+    public List<String> oncallCalendar() {
+        List<String> monthCalendar = new ArrayList<>();
+        String people, month = String.valueOf(this.monthDay.getMonth());
+        Day today;
+        this.dayidx = -1;
+        this.holidayidx = -1;
+        for (int i = 1; i <= this.monthDay.getEndDay(); i++) {
+            today = calendar.get(i).keySet().iterator().next();
+            people = findPeople(i, today);
+            monthCalendar.add(month + "월 " + i + "일 " + today.getDayName() + checkPlusHoliday(i, today) + " " + people + System.lineSeparator());
+            this.prePeople = people;
+        }
+        return monthCalendar;
+    }
+
+    private int findidx(int date, Day today) {
+        if (checkHoliday(date, today)) {
+            this.holidayidx++;
+            if (isholiday) {
+                isholiday = false;
+                return this.holidayidx - 1;
+            }
+            return this.holidayidx;
+        }
+        this.dayidx++;
+        if (isday) {
+            isday = false;
+            return this.dayidx - 1;
+        }
+        return this.dayidx;
+    }
+
+    private String findPeople(int date, Day today) {
+        int idx = findidx(date, today);
+        String people;
+        if (checkHoliday(date, today)) {
+            people = this.holidayPeoples.todayPeople(idx);
+            if (people.equals(prePeople)) {
+                isholiday = true;
+                people = this.holidayPeoples.todayPeople(idx + 1);
+            }
+            return people;
+        }
+        people = this.dayPeoples.todayPeople(idx);
+        if (people.equals(this.prePeople)) {
+            isday = true;
+            people = this.dayPeoples.todayPeople(idx + 1);
+        }
+        return people;
+    }
+
+    private String checkPlusHoliday(int i, Day today) {
+        if (today.isHoliday()) {
+            return "";
+        }
+        if (this.monthDay.checkHoliday(i, today)) {
+            return "(휴일)";
+        }
+        return "";
+    }
+
+    private boolean checkHoliday(int i, Day today) {
+        return this.monthDay.checkHoliday(i, today);
     }
 }
