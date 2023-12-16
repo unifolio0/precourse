2dc66fa079c06aff586e60eada1c3478acaa706e
unifolio0 <boho82ha@gmail.com>
2023-12-16 15:14:39 +0900

    feat : 순번을 관리할 클래스 추가
    - Oncall : 순번을 관리할 클래스

---
+++ src/main/java/oncall/Model/Oncall.java
@@ -0,0 +1,10 @@
+package oncall.Model;
+
+public class Oncall {
+    private Peoples dayPeoples;
+    private Peoples holidayPeoples;
+    public Oncall(Peoples dayPeoples, Peoples holidayPeoples) {
+        this.dayPeoples = dayPeoples;
+        this.holidayPeoples = holidayPeoples;
+    }
+}
